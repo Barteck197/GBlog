f7843ceacccaf9a52b1c2f9e8245e135282bd17d
Grzegorz-linux <gfbardski@outlook.com>
2020-07-16 00:18:22 +0200

    styling the menus

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 get_header();
 
-if (have_posts()) : 
+if (have_posts()) :
     while (have_posts()) : the_post(); ?>
 
     <article class="post">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php the_content(); ?>
+       <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php the_content(); ?>
     </article>
 
 <?php endwhile;
